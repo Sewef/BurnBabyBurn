1b174dcf566d8d81cbdf6200d80a9f3ab73464d7
Sewef <elagroum@gmail.com>
2018-11-11 20:19:57 +0100

    Initial commit

---
+++ src/main/java/com/sewef/burnbabyburn/BabyZombiesBurn.java
@@ -0,0 +1,60 @@
+package com.sewef.burnbabyburn;
+
+import java.util.Random;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.monster.EntityHusk;
+import net.minecraft.entity.item.EntityBoat;
+
+public class BabyZombiesBurn {
+    
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+	}
+     
+     
+	@SubscribeEvent
+	public void entityUpdate(LivingEvent.LivingUpdateEvent event) {
+		EntityZombie zombie;
+		if (event.getEntity() instanceof EntityZombie && !(event.getEntity() instanceof EntityHusk)
+				&& (zombie = (EntityZombie) event.getEntity()).getEntityWorld().isDaytime() && !zombie.getEntityWorld().isRemote && zombie.isChild()) {
+			BlockPos blockpos;
+			float f = zombie.getBrightness();
+			BlockPos blockPos = blockpos = zombie.getRidingEntity() instanceof EntityBoat ? new BlockPos(zombie.posX,
+					(double) Math.round(zombie.posY), zombie.posZ).up() : new BlockPos(zombie.posX, (double) Math.round(zombie.posY), zombie.posZ);
+			if (f > 0.5f && zombie.getEntityWorld().rand.nextFloat() * 30.0f < (f - 0.4f) * 2.0f && zombie.getEntityWorld().canSeeSky(blockpos)) {
+				boolean flag = true;
+				ItemStack itemstack = zombie.getItemStackFromSlot(EntityEquipmentSlot.HEAD);
+				if (!itemstack.isEmpty()) {
+					if (itemstack.isItemStackDamageable()) {
+						itemstack.setItemDamage(itemstack.getItemDamage() + zombie.getEntityWorld().rand.nextInt(2));
+						if (itemstack.getItemDamage() >= itemstack.getMaxDamage()) {
+							zombie.renderBrokenItemStack(itemstack);
+							zombie.setItemStackToSlot(EntityEquipmentSlot.HEAD, ItemStack.EMPTY);
+						}
+					}
+					flag = false;
+				}
+				if (flag) {
+					zombie.setFire(8);
+				}
+			}
+		}
+	}
+
+	public static Object instance;
+
+	public void load(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+}
---
+++ src/main/java/com/sewef/burnbabyburn/MainMod.java
@@ -0,0 +1,28 @@
+package com.sewef.burnbabyburn;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MainMod.MODID, version = MainMod.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
+public class MainMod {
+	public static final String MODID = "burnbabyburn";
+	public static final String VERSION = "1.3";
+
+        private static Logger logger;
+        
+        @EventHandler
+        public void preInit(FMLPreInitializationEvent event)
+        {
+            logger = event.getModLog();
+        }
+        
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+            logger.info("Disco Inferno!");
+            MinecraftForge.EVENT_BUS.register(new BabyZombiesBurn());
+	}
+}
