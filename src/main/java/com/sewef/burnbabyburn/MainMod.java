233af8a5fe4940102bf9699c83e7fe3fc70621a2
Sewef <unconfigured@null.spigotmc.org>
2018-11-21 23:14:38 +0100

    getEntityWorld() => world

--- src/main/java/com/sewef/burnbabyburn/MainMod.java
+++ src/main/java/com/sewef/burnbabyburn/MainMod.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = MainMod.MODID, version = MainMod.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
 public class MainMod {
 	public static final String MODID = "burnbabyburn";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 
         private static Logger logger;
         
